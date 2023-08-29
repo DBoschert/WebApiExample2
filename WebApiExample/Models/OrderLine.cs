79f1278052cdd5df830fee7a77135345040ea20e
Drew Boschert <drew.r.boschert@gmail.com>
2023-08-29 17:00:13 -0400

    Add project files.

---
+++ WebApiExample/Models/OrderLine.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace WebApiExample.Models
+{
+    public class OrderLine
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; } = 1;
+        public int OrderId { get; set; }
+        public virtual Order? Order { get; set; }
+        public int ItemId { get; set; }
+        [JsonIgnore]
+        public virtual Item? Item { get; set; }
+    }
+}
